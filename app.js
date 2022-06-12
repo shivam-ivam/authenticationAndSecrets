95be4059a9664783858458e6f3ee108f8f094915
shivam ivam <ivamanonymous@gmail.com>
2022-06-12 20:15:23 +0530

    level3 - hashing

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 //jshint esversion:6
-require("dotenv").config();
+// require("dotenv").config();
 const express=require("express");
 const bodyParser=require("body-parser")
 const ejs=require("ejs");
 const mongoose=require("mongoose")
-const encrypt=require("mongoose-encryption");
+const md5=require("md5");
 
 const app=express();
 app.use(bodyParser.urlencoded({extended:true}));
@@ -17,8 +17,7 @@ const userSchema=new mongoose.Schema({
     email:String,
     password:String
 });
-
-userSchema.plugin(encrypt,{secret:process.env.SECRET , encryptedFields:['password']}); 
+ 
 
 const User=new mongoose.model("User",userSchema);
 
@@ -38,7 +37,7 @@ app.route("/login")
 .post(function(req,res){
     User.findOne({email:req.body.username},function(err,foundUser){
         if(!err){
-            if(foundUser.password===req.body.password){
+            if(foundUser.password===md5(req.body.password)){
                 res.render("secrets");
             }
         }else{
@@ -56,7 +55,7 @@ app.route("/register")
 .post(function(req,res){
     const newUser=new User({
         email:req.body.username,
-        password:req.body.password
+        password:md5(req.body.password)
     });
     newUser.save(function(err){
         if(!err){
