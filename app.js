5d77239f05d2e11da2fbbd0e787ddfc72cb35b0d
shivam ivam <ivamanonymous@gmail.com>
2022-06-12 15:33:10 +0530

    level2 encryption

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 //jshint esversion:6
+require("dotenv").config();
 const express=require("express");
 const bodyParser=require("body-parser")
 const ejs=require("ejs");
 const mongoose=require("mongoose")
+const encrypt=require("mongoose-encryption");
 
 const app=express();
 app.use(bodyParser.urlencoded({extended:true}));
@@ -11,10 +13,12 @@ app.set('view engine','ejs');
   
 mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser:true});
 
-const userSchema={
+const userSchema=new mongoose.Schema({
     email:String,
     password:String
-};
+});
+
+userSchema.plugin(encrypt,{secret:process.env.SECRET , encryptedFields:['password']}); 
 
 const User=new mongoose.model("User",userSchema);
 
